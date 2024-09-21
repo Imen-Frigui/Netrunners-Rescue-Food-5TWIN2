a09179304767922263eac724155d39d3c97f0460
Imen Frigui <imen_frigui@outlook.com>
2024-09-21 13:12:37 +0100

    template

---
+++ resources/views/sessions/create.blade.php
@@ -0,0 +1,114 @@
+<x-layout bodyClass="bg-gray-200">
+
+        <div class="container position-sticky z-index-sticky top-0">
+            <div class="row">
+                <div class="col-12">
+                    <!-- Navbar -->
+                    <x-navbars.navs.guest signin='login' signup='register'></x-navbars.navs.guest>
+                    <!-- End Navbar -->
+                </div>
+            </div>
+        </div>
+        <main class="main-content  mt-0">
+            <div class="page-header align-items-start min-vh-100"
+                style="background-image: url('https://attachments.office.net/owa/Imen.Frigui%40esprit.tn/service.svc/s/GetAttachmentThumbnail?id=AAMkADhmOGEyOTU1LTY1ZGItNGVlNy1iYjMxLTU2MzkwZDVhOGRkZQBGAAAAAAAX1eX35ISwQ5GNiKxP0FGvBwBBPUF1QI8hQbG3hwb1v0fJAAAAAAEMAABBPUF1QI8hQbG3hwb1v0fJAADbJBl2AAABEgAQAHnFktlPPilGpH1%2Bu3Ucx8c%3D&thumbnailType=2&token=eyJhbGciOiJSUzI1NiIsImtpZCI6IkU1RDJGMEY4REE5M0I2NzA5QzQzQTlFOEE2MTQzQzAzRDYyRjlBODAiLCJ0eXAiOiJKV1QiLCJ4NXQiOiI1ZEx3LU5xVHRuQ2NRNm5vcGhROEE5WXZtb0EifQ.eyJvcmlnaW4iOiJodHRwczovL291dGxvb2sub2ZmaWNlLmNvbSIsInVjIjoiMjk3Y2E4ZWUzMGIyNDBlOWEyZGZjNWEwN2ZlMGFkYTciLCJzaWduaW5fc3RhdGUiOiJrbXNpIiwidmVyIjoiRXhjaGFuZ2UuQ2FsbGJhY2suVjEiLCJhcHBjdHhzZW5kZXIiOiJPd2FEb3dubG9hZEA2MDRmMWE5Ni1jYmU4LTQzZjgtYWJiZi1mOGVhZjVkODU3MzAiLCJpc3NyaW5nIjoiV1ciLCJhcHBjdHgiOiJ7XCJtc2V4Y2hwcm90XCI6XCJvd2FcIixcInB1aWRcIjpcIjExNTM4MDExMjY5NTc2MDQ5NjBcIixcInNjb3BlXCI6XCJPd2FEb3dubG9hZFwiLFwib2lkXCI6XCJmMTliYjUxNS04NDc4LTQwZGYtODUyYS02NmZlZjJjNmI2NDlcIixcInByaW1hcnlzaWRcIjpcIlMtMS01LTIxLTMzMzc0NTUzNjgtMTk2MTk0ODg2OS0xMzQ1NDcwMzA1LTEwNjM1Mjg0XCJ9IiwibmJmIjoxNzI2OTIwMzUwLCJleHAiOjE3MjY5MjA2NTAsImlzcyI6IjAwMDAwMDAyLTAwMDAtMGZmMS1jZTAwLTAwMDAwMDAwMDAwMEA2MDRmMWE5Ni1jYmU4LTQzZjgtYWJiZi1mOGVhZjVkODU3MzAiLCJhdWQiOiIwMDAwMDAwMi0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAvYXR0YWNobWVudHMub2ZmaWNlLm5ldEA2MDRmMWE5Ni1jYmU4LTQzZjgtYWJiZi1mOGVhZjVkODU3MzAiLCJoYXBwIjoib3dhIn0.ro6q_SHWZR8GqMTPqrIyLSZFUGTLhpBimzIQ_9XVJyBKLgA8bDayyKDHr6ixxlOQG2GWO4tZo5H8QXAPjtw50qeCXJeh7fkWNso8GT9tVYEWXGLUZXeQ4BfsKrM5m4Q5c95Bad17LOXu4Yw_N94rSpZVa7G2Fck3s73DxL3-yLaUDnbPU4C9yunqBo9DcB_Cl6eY5DUVqHc8ATYD23U2M90Mpp-RJfnnX4Dm3Q-_b-Pfb3nZkOgi3GqQskMQ21-I7ypCw-oFfNoW1in_joWOPQ8VMzyjDrPZoZP-g45Ci1jSVpqZDXy1yLjhoIMV0b7hEmJuZKeLKVfr-W8xs4LBdw&X-OWA-CANARY=bdvoVxp9pqcAAAAAAAAAAEDfLco12twYNBjswF8N-itd8NhXLv_ytd91jnSbUEnnEvLtHZ08cts.&owa=outlook.office.com&scriptVer=20240906004.20&clientId=C19CE05FB8BA4DFE85DB7A0FEFDE1410&animation=true');">
+                <span class="mask bg-gradient-dark opacity-6"></span>
+                <div class="container mt-5">
+                    <div class="row signin-margin">
+                        <div class="col-lg-4 col-md-8 col-12 mx-auto">
+                            <div class="card z-index-0 fadeIn3 fadeInBottom">
+                                <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
+                                    <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
+                                        <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">Sign in</h4>
+                                        <div class="row mt-3">
+                                            <h6 class='text-white text-center'>
+                                                <span class="font-weight-normal">Email:</span> admin@material.com
+                                                <br>
+                                                <span class="font-weight-normal">Password:</span> secret</h6>
+                                            <div class="col-2 text-center ms-auto">
+                                                <a class="btn btn-link px-3" href="javascript:;">
+                                                    <i class="fa fa-facebook text-white text-lg"></i>
+                                                </a>
+                                            </div>
+                                            <div class="col-2 text-center px-1">
+                                                <a class="btn btn-link px-3" href="javascript:;">
+                                                    <i class="fa fa-github text-white text-lg"></i>
+                                                </a>
+                                            </div>
+                                            <div class="col-2 text-center me-auto">
+                                                <a class="btn btn-link px-3" href="javascript:;">
+                                                    <i class="fa fa-google text-white text-lg"></i>
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card-body">
+                                    <form role="form" method="POST" action="{{ route('login') }}" class="text-start">
+                                        @csrf
+                                        @if (Session::has('status'))
+                                        <div class="alert alert-success alert-dismissible text-white" role="alert">
+                                            <span class="text-sm">{{ Session::get('status') }}</span>
+                                            <button type="button" class="btn-close text-lg py-3 opacity-10"
+                                                data-bs-dismiss="alert" aria-label="Close">
+                                                <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        @endif
+                                        <div class="input-group input-group-outline mt-3">
+                                            <label class="form-label">Email</label>
+                                            <input type="email" class="form-control" name="email" value="{{ 'admin@material.com' }}">
+                                        </div>
+                                        @error('email')
+                                        <p class='text-danger inputerror'>{{ $message }} </p>
+                                        @enderror
+                                        <div class="input-group input-group-outline mt-3">
+                                            <label class="form-label">Password</label>
+                                            <input type="password" class="form-control" name="password" value='{{ 'secret'}}'>
+                                        </div>
+                                        @error('password')
+                                        <p class='text-danger inputerror'>{{ $message }} </p>
+                                        @enderror
+                                        <div class="form-check form-switch d-flex align-items-center my-3">
+                                            <input class="form-check-input" type="checkbox" id="rememberMe">
+                                            <label class="form-check-label mb-0 ms-2" for="rememberMe">Remember
+                                                me</label>
+                                        </div>
+                                        <div class="text-center">
+                                            <button type="submit" class="btn bg-gradient-primary w-100 my-4 mb-2">Sign
+                                                in</button>
+                                        </div>
+                                        <p class="mt-4 text-sm text-center">
+                                            Don't have an account?
+                                            <a href="{{ route('register') }}"
+                                                class="text-primary text-gradient font-weight-bold">Sign up</a>
+                                        </p>
+                                        <p class="text-sm text-center">
+                                            Forgot your password? Reset your password
+                                            <a href="{{ route('verify') }}"
+                                                class="text-primary text-gradient font-weight-bold">here</a>
+                                        </p>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <x-footers.guest></x-footers.guest>
+            </div>
+        </main>
+        @push('js')
+<script src="{{ asset('assets') }}/js/jquery.min.js"></script>
+<script>
+    $(function() {
+
+    var text_val = $(".input-group input").val();
+    if (text_val === "") {
+      $(".input-group").removeClass('is-filled');
+    } else {
+      $(".input-group").addClass('is-filled');
+    }
+});
+</script>
+@endpush
+</x-layout>
