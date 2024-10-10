d8d9edf81e2508c562075791f849beaf1b906fd6
Imen Frigui <imen_frigui@outlook.com>
2024-10-10 22:06:08 +0100

    refactor:mergin events; fixing sidebar

--- resources/views/components/navbars/sidebar.blade.php
+++ resources/views/components/navbars/sidebar.blade.php
@@ -3,86 +3,34 @@
 <aside
     class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 bg-gradient-dark"
     id="sidenav-main">
-    
+
     <!-- Sidebar Header with Logo -->
     <div class="sidenav-header">
-
         <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
-            aria-hidden="true" id="iconSidenav"></i>
-            <a class="navbar-brand m-0 d-flex text-wrap align-items-center" href="{{ route('dashboard') }}">
-            <img src="{{ asset('assets/img/rescuefood.svg') }}" alt="Rescue Food Logo" style="width: 1000px; height: auto;">
-            </a>
-<!-- Include Font Awesome 5 -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+        aria-hidden="true" id="iconSidenav"></i>
+    <a class="navbar-brand m-0 d-flex text-wrap align-items-center" href="{{ route('dashboard') }}"
+        style="max-height: 120px; display: block;">
+        <img src="{{ asset('assets') }}/img/rescuefood.svg" class="navbar-brand-img" alt="logo1"
+            style="max-height: 2000px; margin-bottom:40px">
+    </a>
+
+        <!-- Include Font Awesome 5 -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
     </div>
 
-    <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
-        aria-hidden="true" id="iconSidenav"></i>
-    <a class="navbar-brand m-0 d-flex text-wrap align-items-center" href="{{ route('dashboard') }}"
-       style="max-height: 120px; display: block;"> 
-        <img src="{{ asset('assets') }}/img/logo3.png" class="navbar-brand-img" alt="logo1"
-             style="max-height: 110px; width: auto; display: block; margin-bottom:30px;"> 
-    </a>
-</div>
+
+    </div>
 
 
     <hr class="horizontal light mt-0 mb-2">
-    
+
     <div class="collapse navbar-collapse w-auto max-height-vh-100" id="sidenav-collapse-main">
         <ul class="navbar-nav">
 
             <!-- Dashboard Route - Now on Top -->
             <li class="nav-item mt-3">
-                <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Laravel examples</h6>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-white {{ $activePage == 'user-profile' ? 'active bg-gradient-success' : '' }} "
-                    href="{{ route('user-profile') }}">
-                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-                        <i style="font-size: 1.2rem;" class="fas fa-user-circle ps-2 pe-2 text-center"></i>
-                    </div>
-                    <span class="nav-link-text ms-1">User Profile</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-white {{ $activePage == 'events' ? ' active bg-gradient-primary' : '' }} "
-                    href="{{ route('events.index') }}">
-                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-                        <i style="font-size: 1rem;" class="fas fa-lg fa-list-ul ps-2 pe-2 text-center"></i>
-                    </div>
-                    <span class="nav-link-text ms-1">Event   Management</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-white {{ $activePage == 'user-management' ? ' active bg-gradient-success' : '' }} "
-                    href="{{ route('user-management') }}">
-                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-                        <i style="font-size: 1rem;" class="fas fa-lg fa-list-ul ps-2 pe-2 text-center"></i>
-                    </div>
-                    <span class="nav-link-text ms-1">User Management</span>
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-white {{ $activePage == 'charities' ? ' active bg-gradient-success' : ''}} "
-                    href="{{ route('charities') }}">
-                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-                        <i style="font-size: 1rem;" class="fas fa-solid fa-hand-holding-heart ps-2 pe-2 text-center"></i>
-                    </div>
-                    <span class="nav-link-text ms-1">Charities</span>
-                </a>
-            </li>
-                <a class="nav-link text-white {{ $activePage == 'pickup-management' ? ' active bg-gradient-primary' : '' }} "
-                    href="{{ route('pickup-management') }}">
-                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-                        <i style="font-size: 1rem;" class="fas fa-lg fa-list-ul ps-2 pe-2 text-center"></i>
-                    </div>
-                    <span class="nav-link-text ms-1">PickUps Management</span>
-                </a>
-            </li>
-
-            <li class="nav-item mt-3">
-                <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Pages</h6>
+                <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8"></h6>
             </li>
             <li class="nav-item">
                 <a class="nav-link text-white {{ $activePage == 'dashboard' ? ' active bg-gradient-primary' : '' }} "
@@ -96,7 +44,8 @@
 
             <!-- Laravel Examples Section -->
             <li class="nav-item mt-3">
-                <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">User Profile Section</h6>
+                <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">User Profile
+                    Section</h6>
             </li>
 
             <!-- User Profile Route -->
@@ -110,35 +59,73 @@
                 </a>
             </li>
 
-            <!-- User Management Route -->
-            <li class="nav-item">
-                <a class="nav-link text-white {{ $activePage == 'user-management' ? ' active bg-gradient-primary' : '' }} "
-                    href="{{ route('user-management') }}">
-                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-                        <i class="fas fa-lg fa-list-ul ps-2 pe-2 text-center" style="font-size: 1rem;"></i>
-                    </div>
-                    <span class="nav-link-text ms-1">User Management</span>
-                </a>
-            </li>
 
+            <li class="nav-item mt-3">
+                <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Pages</h6>
+            </li>
+           
             <!-- Pages Section -->
             <li class="nav-item mt-3">
-                <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Management Section</h6>
+                <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-8">Management Section
+                </h6>
+                <li class="nav-item">
+                    <a class="nav-link text-white {{ $activePage == 'events' ? ' active bg-gradient-primary' : '' }} "
+                        href="{{ route('events.index') }}">
+                        <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                            <i class="material-icons opacity-10">notifications</i>
+                        </div>
+                        <span class="nav-link-text ms-1">Event Management</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white {{ $activePage == 'charities' ? ' active bg-gradient-success' : '' }} "
+                        href="{{ route('charities') }}">
+                        <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                            <i style="font-size: 1rem;"
+                                class="fas fa-solid fa-hand-holding-heart text-center"></i>
+                        </div>
+                        <span class="nav-link-text ms-1">Charities</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white {{ $activePage == 'pickup-management' ? ' active bg-gradient-primary' : '' }} "
+                        href="{{ route('pickup-management') }}">
+                        <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                            <i class="fas fa-lg fa-list-ul text-center"></i>
+                        </div>
+                        <span class="nav-link-text ms-1">PickUps Management</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white {{ $activePage == 'restaurants' ? ' active bg-gradient-primary' : '' }}"
+                        href="{{ route('restaurants') }}">
+                        <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                            <i class="material-icons opacity-10">receipt_long</i>
+                        </div>
+                        <span class="nav-link-text ms-1">Restaurants Manafement</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white {{ $activePage == 'food-management' ? ' active bg-gradient-primary' : '' }} "
+                        href="{{ route('foods.index') }}">
+                        <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                            <i class="fas fa-utensils"></i>
+                        </div>
+                        <span class="nav-link-text ms-1">Food Management</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white {{ $activePage == 'user-management' ? ' active bg-gradient-success' : '' }} "
+                        href="{{ route('user-management') }}">
+                        <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                            <i class="fas fa-lg fa-list-ul text-center"></i>
+                        </div>
+                        <span class="nav-link-text ms-1">User Management</span>
+                    </a>
+                </li>
             </li>
-
-            <!-- Food Management Route -->
-            <li class="nav-item">
-                <a class="nav-link text-white {{ $activePage == 'food-management' ? ' active bg-gradient-primary' : '' }} "
-                    href="{{ route('foods.index') }}">
-                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-                        <i class="fas fa-utensils" style="font-size: 1.2rem;"></i>
-                    </div>
-                    <span class="nav-link-text ms-1">Food Management</span>
-                </a>
-            </li>
-
             <!-- Other existing pages -->
-            <li class="nav-item">
+            {{-- <li class="nav-item">
                 <a class="nav-link text-white {{ $activePage == 'tables' ? ' active bg-gradient-primary' : '' }} "
                     href="{{ route('tables') }}">
                     <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
@@ -156,18 +143,10 @@
                     </div>
                     <span class="nav-link-text ms-1">Billing</span>
                 </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link text-white {{ $activePage == 'restaurants' ? ' active bg-gradient-primary' : '' }}"
-                    href="{{ route('restaurants') }}">
-                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
-                        <i class="material-icons opacity-10">receipt_long</i>
-                    </div>
-                    <span class="nav-link-text ms-1">Restaurants</span>
-                </a>
-            </li>
+            </li> --}}
+            
 
-            <li class="nav-item">
+            {{-- <li class="nav-item">
                 <a class="nav-link text-white {{ $activePage == 'virtual-reality' ? ' active bg-gradient-primary' : '' }} "
                     href="{{ route('virtual-reality') }}">
                     <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
@@ -175,9 +154,9 @@
                     </div>
                     <span class="nav-link-text ms-1">Virtual Reality</span>
                 </a>
-            </li>
+            </li> --}}
 
-            <li class="nav-item">
+            {{-- <li class="nav-item">
                 <a class="nav-link text-white {{ $activePage == 'rtl' ? ' active bg-gradient-primary' : '' }} "
                     href="{{ route('rtl') }}">
                     <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
@@ -185,9 +164,9 @@
                     </div>
                     <span class="nav-link-text ms-1">RTL</span>
                 </a>
-            </li>
+            </li> --}}
 
-            <li class="nav-item">
+            {{-- <li class="nav-item">
                 <a class="nav-link text-white {{ $activePage == 'notifications' ? ' active bg-gradient-primary' : '' }} "
                     href="{{ route('notifications') }}">
                     <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
@@ -195,7 +174,7 @@
                     </div>
                     <span class="nav-link-text ms-1">Notifications</span>
                 </a>
-            </li>
+            </li> --}}
 
             <!-- Account Pages Section -->
             <li class="nav-item mt-3">
@@ -212,7 +191,7 @@
                 </a>
             </li>
 
-            <li class="nav-item">
+            {{-- <li class="nav-item">
                 <a class="nav-link text-white" href="{{ route('static-sign-in') }}">
                     <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
                         <i class="material-icons opacity-10">login</i>
@@ -228,7 +207,7 @@
                     </div>
                     <span class="nav-link-text ms-1">Sign Up</span>
                 </a>
-            </li>
+            </li> --}}
         </ul>
     </div>
 </aside>
