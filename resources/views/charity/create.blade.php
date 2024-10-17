127e71dac5fa09a6d08e8916b2c84967ae50872a
Imen Frigui <imen_frigui@outlook.com>
2024-10-18 00:14:17 +0100

    refact: add function;

--- resources/views/charity/create.blade.php
+++ resources/views/charity/create.blade.php
@@ -4,6 +4,16 @@
         <x-navbars.navs.auth titlePage="Add New Charity"></x-navbars.navs.auth>
         <div class="container-fluid py-4">
             <div class="card my-4">
+                @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
                 <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                     <div class="bg-gradient-success shadow-success border-radius-lg pt-4 pb-3">
                         <h6 class="text-white text-capitalize ps-3">Add New Charity</h6>
